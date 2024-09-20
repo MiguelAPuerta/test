c3af084052228155da2cbb5e819fb3691fcbd50e
Miguel Angel <miguel.puerta@udea.edu.co>
2024-09-20 23:13:13 +0900

    test

---
+++ src/main/java/com/udea/vueloudea/model/City.java
@@ -0,0 +1,74 @@
+package com.udea.vueloudea.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class City {
+
+    @Id
+    private String iataCode;
+
+    private String name;
+    private String country;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<Airport> airports = new ArrayList<>();;
+
+    public City() {
+    }
+
+    public City(String iataCode, String name, String country) {
+        this.iataCode = iataCode;
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+
+    public void setIataCode(String iataCode) {
+        this.iataCode = iataCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(getIataCode(), city.getIataCode());
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
